96ab20cefc86a0a6878090a35895b3fe4ddfa8ba
Kostas Bariotis <konmpar@gmail.com>
2023-08-20 17:17:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ DIR_NAME="config"
 ALIAS_NAME="config"
 
 git clone --separate-git-dir=$HOME/.$DIR_NAME https://github.com/kbariotis/dotfiles $HOME/myconf-tmp
-cp ~/myconf-tmp/.gitmodules ~
+
 rm -r ~/myconf-tmp/
+
 alias $ALIAS_NAME="/usr/bin/git --git-dir=$HOME/.$DIR_NAME/ --work-tree=$HOME"
+
+config config status.showUntrackedFiles no
